7d5d64380fe898d13071e47db602cdac88169a97
Gustavo Alexandre <gassantos@id.uff.br>
2026-02-17 18:33:09 -0300

    Fix: replace os.path with pathlib for path handling in JsonFromFiles and FilenameOnly datasets

--- dataset/others/FilenameOnly.py
+++ dataset/others/FilenameOnly.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from torch.utils.data import Dataset
 
 from tools.dataset_tool import dfs_search
@@ -9,13 +9,13 @@ class FilenameOnlyDataset(Dataset):
         self.config = config
         self.mode = mode
         self.file_list = []
-        self.data_path = config.get("data", "%s_data_path" % mode)
+        self.data_path = Path(config.get("data", "%s_data_path" % mode))
 
         filename_list = config.get("data", "%s_file_list" % mode).replace(" ", "").split(",")
         recursive = config.getboolean("data", "recursive")
 
         for name in filename_list:
-            self.file_list = self.file_list + dfs_search(os.path.join(self.data_path, name), recursive)
+            self.file_list = self.file_list + dfs_search(self.data_path / name, recursive)
         self.file_list.sort()
 
     def __getitem__(self, item):
